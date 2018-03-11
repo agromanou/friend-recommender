3cbf2e9eabff45421c69933fe179656295799407
eraikakou <t.raikakou@gmail.com>
2018-03-11 16:25:27 +0200

    compute the similarity percentage

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from recommendations import Recommendations
 if __name__ == '__main__':
 
     file = '/Users/aggrom/Desktop/MSDS/5_Data_mining/Assignment_1/friend-recommender/data/facebook_combined.txt'
+    percentages = []
     algo_list = ['common_neighbors', 'jaccard', 'adamic_adar', 'cosine']
 
     fetcher = DataFetcher(file)
@@ -14,6 +15,20 @@ if __name__ == '__main__':
 
     recommender.compute_the_number_users_with_the_same_first_and_different_10_recommendations()
 
+    percentages.append(recommender.compute_similarity_percentage('common_neighbors', 'jaccard'))
+    percentages.append(recommender.compute_similarity_percentage('common_neighbors', 'adamic_adar'))
+    percentages.append(recommender.compute_similarity_percentage('jaccard', 'adamic_adar'))
+    percentages.append(recommender.compute_similarity_percentage('cosine', 'common_neighbors'))
+    percentages.append(recommender.compute_similarity_percentage('cosine', 'jaccard'))
+    percentages.append(recommender.compute_similarity_percentage('cosine', 'adamic_adar'))
+
+    total = 0
+    for percentage in percentages:
+        total = total + percentage
+
+    average_similarity = round((total/ len(percentages)), 2)
+    print('The average similarity between the algorithms is: ' + str(average_similarity))
+    
     for algo in algo_list:
         print()
         print()
